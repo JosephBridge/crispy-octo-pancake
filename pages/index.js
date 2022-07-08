3247a2a155abcf9094e1c7874c3fd2ea9a8576db
JosephBridge <90578379+JosephBridge@users.noreply.github.com>
2022-07-08 10:59:25 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import FeedbackForm from "@components/FeedbackForm";
+import JokeBlock from "@components/JokeBlock";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Toolbox</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Next.js Toolbox" />
+        <hr />
+        <p className="description">
+          Here's an example of a Netlify Form! When you fill this out, the
+          submissions can be found in the Netlify Admin site.
+        </p>
+        <FeedbackForm />
+        <JokeBlock />
+      </main>
+      <Footer />
+    </div>
+  );
+}
